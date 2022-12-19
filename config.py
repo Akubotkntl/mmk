401a0f3bb68e506a8b36e399c06bdf8d00f10684
Akubotkntl <120328345+Akubotkntl@users.noreply.github.com>
2022-12-20 01:46:56 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from os import getenv
 API_ID = int(getenv("API_ID", "6435225")) #optional
 API_HASH = getenv("API_HASH", "") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5719966016").split()))
+OWNER_ID = int(getenv("OWNER_ID", "5719966016"))
+MONGO_URL = getenv("MONGO_URL, "mongodb+srv://Jalu:jalu@cluster0.enrwq7p.mongodb.net/?retryWrites=true&w=majority")
+BOT_TOKEN = getenv("BOT_TOKEN", "5959882409:AAFlJ6virBm6dmFHem593pP3tO7oGFyOSIg")
 ALIVE_PIC = getenv("ALIVE_PIC")
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
@@ -15,7 +15,7 @@ GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
 BRANCH = getenv("BRANCH", "master") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "BQBiMZkAWwYze4MSk6XQ3ynHqdf3U__7rRBxQumEfskpmS6MLjoUaxjh0SrSDHQoSHWmdxzg0hilIRSldh2CZPH_wsgtYnvkflg2rsIxfwmgea_Hcgp5cJ7c8J2bvCjVKWNCMBVBXnh-rrk6okrDUbUKunzL-6ZNzYJ2DlDkUkcx5WvLCIG8j7h4YWvIK5vc4bPhEfMTCfnA1i2zS-RQQV-rnj_Eozlyh5CnmwZxjHQaBG462lK4Pew5fSmFStKp2vcESlkuF1o3xpFqk9C-rsKk2PrhYTZC4KVrbdBYP5ltsLp55jHNphJgPk13uFolNuuRVIN7hdjZx7iOKm7lwNe87beBPAAAAAFU78FAAA")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
